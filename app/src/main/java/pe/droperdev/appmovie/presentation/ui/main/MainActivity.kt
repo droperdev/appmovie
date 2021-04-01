2c67caa4f4ebeccbca09079b4100ea672a67ada5
Pedro Lumbre <1111819@utp.edu.pe>
2021-04-01 11:33:15 -0500

    configuracion arquitectura

--- app/src/main/java/pe/droperdev/appmovie/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/pe/droperdev/appmovie/presentation/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package pe.droperdev.appmovie
+package pe.droperdev.appmovie.presentation.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pe.droperdev.appmovie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
